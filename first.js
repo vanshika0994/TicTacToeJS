1009c8583cd8b9a940b513b13a8ab43551f94bae
vanshika0994 <vanshika3918@gmail.com>
2025-05-10 10:18:17 +0530

    updated O color to gold

---
+++ first.js
@@ -0,0 +1,11 @@
+//Ques
+const profile = {
+    name: 'ShradhaKhapra',
+    isfollow: true,
+    posts: 195,
+    followers: 569,
+    following: 4,
+    bio: 'Entrepreneur'
+
+};
+console.log(profile);
